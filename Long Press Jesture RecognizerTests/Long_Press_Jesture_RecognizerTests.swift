9e8d4293e5dafc3b00a21f1e8306b6caed1f9678
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-01-17 11:06:53 -0600

    Initial Commit

---
+++ Long Press Jesture RecognizerTests/Long_Press_Jesture_RecognizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Long_Press_Jesture_RecognizerTests.swift
+//  Long Press Jesture RecognizerTests
+//
+//  Created by Sam Burrell on 1/17/17.
+//  Copyright © 2017 Sam Burrell. All rights reserved.
+//
+
+import XCTest
+@testable import Long_Press_Jesture_Recognizer
+
+class Long_Press_Jesture_RecognizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
